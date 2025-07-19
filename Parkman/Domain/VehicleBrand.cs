451baae3f3514c050d66279e4ec663475a091320
Gh0stbreaker <tomas.vanek@vancloud.cz>
2025-07-19 13:14:05 +0200

    přidání značek do enumu

--- Parkman/Domain/VehicleBrand.cs
+++ Parkman/Domain/VehicleBrand.cs
@@ -1,11 +1,166 @@
-namespace Parkman.Domain;
-
-public enum VehicleBrand
+﻿namespace Parkman.Domain
 {
-    Tesla,
-    Toyota,
-    Ford,
-    Bmw,
-    Mercedes,
-    Honda
+    public enum VehicleBrand
+    {
+        // 🇺🇸 USA
+        Tesla,
+        Ford,
+        Chevrolet,
+        Chrysler,
+        Dodge,
+        Jeep,
+        Cadillac,
+        GMC,
+        Lincoln,
+        Buick,
+        Hummer,
+        Ram,
+
+        // 🇩🇪 Německo
+        Bmw,
+        MercedesBenz,
+        Audi,
+        Volkswagen,
+        Porsche,
+        Opel,
+        Smart,
+        Maybach,
+
+        // 🇫🇷 Francie
+        Peugeot,
+        Citroen,
+        Renault,
+        DS,
+
+        // 🇮🇹 Itálie
+        Fiat,
+        AlfaRomeo,
+        Lancia,
+        Ferrari,
+        Lamborghini,
+        Maserati,
+        Pagani,
+        Iveco,
+
+        // 🇬🇧 Velká Británie
+        Jaguar,
+        LandRover,
+        AstonMartin,
+        Bentley,
+        RollsRoyce,
+        Mini,
+        Lotus,
+        MG,
+        Vauxhall,
+        Rover,
+
+        // 🇪🇸 Španělsko
+        Seat,
+        Cupra,
+
+        // 🇨🇿 Česká republika
+        Skoda,
+
+        // 🇸🇪 Švédsko
+        Volvo,
+        Saab,
+        Koenigsegg,
+
+        // 🇳🇱 Nizozemsko
+        Spyker,
+        DAF,
+
+        // 🇷🇴 Rumunsko
+        Dacia,
+
+        // 🇨🇭 Švýcarsko
+        Rinspeed,
+
+        // 🇭🇺 Maďarsko
+        MagyarSuzuki, // výrobce Suzuki v Evropě
+
+        // 🇭🇷 Chorvatsko
+        Rimac,
+
+        // 🇯🇵 Japonsko
+        Toyota,
+        Honda,
+        Nissan,
+        Mazda,
+        Subaru,
+        Suzuki,
+        Mitsubishi,
+        Lexus,
+        Infiniti,
+        Daihatsu,
+        Acura,
+
+        // 🇰🇷 Jižní Korea
+        Hyundai,
+        Kia,
+        Genesis,
+        SsangYong,
+
+        // 🇨🇳 Čína
+        Geely,
+        Chery,
+        BYD,
+        Nio,
+        XPeng,
+        GreatWall,
+        Haval,
+        Aiways,
+        Hongqi,
+        LynkAndCo,
+        Roewe,
+        Wuling,
+        Baojun,
+        Zeekr,
+
+        // 🇮🇳 Indie
+        Tata,
+        Mahindra,
+        MarutiSuzuki,
+        AshokLeyland,
+
+        // 🇲🇾 Malajsie
+        Proton,
+        Perodua,
+
+        // 🇹🇷 Turecko
+        Tofas,
+        TOGG,
+
+        // 🇷🇺 Rusko
+        Lada,
+        UAZ,
+        GAZ,
+        Moskvitch,
+
+        // 🇦🇺 Austrálie
+        Holden,
+
+        // 🇧🇷 Brazílie
+        Troller,
+
+        // 🇦🇷 Argentina
+        IKA,
+
+        // Ostatní / exotičtí výrobci
+        Bugatti,      // původem Francie, vlastněná Rimacem
+        DeTomaso,
+        Fisker,
+        Lucid,
+        Polestar,
+        Canoo,
+        Rivian,
+        Bollinger,
+        FaradayFuture,
+        Aptera,
+        Scania,       // nákladní vozy (Švédsko)
+        MAN,          // nákladní vozy (Německo)
+        Kamaz,        // nákladní vozy (Rusko)
+        Isuzu,
+        Hino
+    }
 }
