1d1388c23bcf733a87e9f258c228a40abb1c4e6c
Gh0stbreaker <46496187+Gh0stbreaker@users.noreply.github.com>
2025-07-20 20:26:57 +0200

    Add Swagger version configuration

--- Parkman/Program.cs
+++ Parkman/Program.cs
@@ -29,7 +29,14 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Parkman API",
+        Version = "v1"
+    });
+});
 builder.Services.AddRepositories();
 builder.Services.AddServices();
 
