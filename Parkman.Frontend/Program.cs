3806ec5fa171b70999fd3746c3abee5b925866bc
Gh0stbreaker <tomas.vanek@vancloud.cz>
2025-07-20 20:16:59 +0200

    test

--- Parkman.Frontend/Program.cs
+++ Parkman.Frontend/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
 
 namespace Parkman.Frontend;
 
