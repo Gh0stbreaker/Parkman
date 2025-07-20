5b54f6c437133edb8664920aecedfa7bec4ceff8
Gh0stbreaker <46496187+Gh0stbreaker@users.noreply.github.com>
2025-07-20 15:10:04 +0200

    Add generic repository with advanced query options

---
+++ Parkman/Infrastructure/Repositories/IGenericRepository.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Parkman.Infrastructure.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity?> GetByIdAsync(object id);
+
+        Task<IReadOnlyList<TEntity>> ListAsync(
+            Expression<Func<TEntity, bool>>? filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+            string includeProperties = "",
+            int? skip = null,
+            int? take = null);
+
+        Task<TEntity> AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+    }
+}
