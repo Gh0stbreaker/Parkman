c5a5383a35876b926313c03e04d4365241d1cd0a
Gh0stbreaker <46496187+Gh0stbreaker@users.noreply.github.com>
2025-07-22 12:19:44 +0200

    Add transactional registration

--- Parkman/Infrastructure/Services/UserVehicleRegistrationService.cs
+++ Parkman/Infrastructure/Services/UserVehicleRegistrationService.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Parkman.Domain.Entities;
 using Parkman.Domain.Enums;
 using Parkman.Infrastructure.Repositories.Entities;
+using Parkman.Infrastructure;
 
 namespace Parkman.Infrastructure.Services;
 
@@ -27,15 +28,18 @@ public class UserVehicleRegistrationService : IUserVehicleRegistrationService
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly IPersonProfileRepository _personRepo;
     private readonly IVehicleRepository _vehicleRepo;
+    private readonly ApplicationDbContext _context;
 
     public UserVehicleRegistrationService(
         UserManager<ApplicationUser> userManager,
         IPersonProfileRepository personRepo,
-        IVehicleRepository vehicleRepo)
+        IVehicleRepository vehicleRepo,
+        ApplicationDbContext context)
     {
         _userManager = userManager;
         _personRepo = personRepo;
         _vehicleRepo = vehicleRepo;
+        _context = context;
     }
 
     public async Task<IdentityResult> RegisterAsync(
@@ -52,21 +56,34 @@ public class UserVehicleRegistrationService : IUserVehicleRegistrationService
         VehiclePropulsionType propulsionType,
         bool shareable = false)
     {
+        using var transaction = await _context.Database.BeginTransactionAsync();
+
         var user = new ApplicationUser { UserName = email, Email = email };
         var createResult = await _userManager.CreateAsync(user, password);
         if (!createResult.Succeeded)
         {
+            await transaction.RollbackAsync();
             return createResult;
         }
 
-        var profile = new PersonProfile(firstName, lastName, dateOfBirth, phoneNumber, address);
-        user.SetPersonProfile(profile);
-        await _personRepo.AddAsync(profile);
+        try
+        {
+            var profile = new PersonProfile(firstName, lastName, dateOfBirth, phoneNumber, address);
+            user.SetPersonProfile(profile);
+            await _personRepo.AddAsync(profile);
 
-        var vehicle = new Vehicle(licensePlate, brand, type, propulsionType, shareable);
-        profile.SetVehicle(vehicle);
-        await _vehicleRepo.AddAsync(vehicle);
+            var vehicle = new Vehicle(licensePlate, brand, type, propulsionType, shareable);
+            profile.SetVehicle(vehicle);
+            await _vehicleRepo.AddAsync(vehicle);
 
-        return createResult;
+            await transaction.CommitAsync();
+            return createResult;
+        }
+        catch
+        {
+            await transaction.RollbackAsync();
+            await _userManager.DeleteAsync(user);
+            throw;
+        }
     }
 }
